39998771339f5854a47c7dc77cc92b5225f4798a
bubz <bao.es.hoang2@gmail.com>
2020-08-21 19:09:38 +1000

    allow react to serve built react files

--- express/server.js
+++ express/server.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // serving static files
+app.use(express.static(path.resolve(__dirname, '../react', 'build')));
 
 // api imports -------------------------------------------
 const sample = require('./routes/sample');
@@ -24,6 +25,9 @@ app.use('/api/v1/sample', sample);
 // -------------------------------------------------------
 
 // default redirect to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../react', 'build', 'index.html'))
+})
 
 // start express server
 app.listen(PORT, () => {
